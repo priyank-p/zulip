51850e3c29779a6ec051e4950d559262fe40999a
Dinesh <chdinesh1089@gmail.com>
2020-06-01 00:44:12 +0530

    02 with functions needed

--- frontend_tests/puppeteer_lib/common.js
+++ frontend_tests/puppeteer_lib/common.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const puppeteer = require('puppeteer');
 const assert = require("assert");
+const test_credentials = require('../../var/casper/test_credentials.js').test_credentials;
+
 
 class CommonUtils {
     constructor() {
         this.browser = null;
         this.screenshot_id = 0;
         this.realm_url = "http://zulip.zulipdev.com:9981/";
+        this.last_send_or_update = -1;
+    }
+
+    timestamp() {
+        return new Date().getTime();
     }
 
     async ensure_browser() {
@@ -46,6 +53,11 @@ class CommonUtils {
         });
     }
 
+    async set_pm_recipient(page, recipient) {
+        await page.type("#private_message_recipient", recipient);
+        await page.keyboard.press("Enter");
+    }
+
     /**
      * This function takes a params object whose fields
      * are referenced by name attribute of an input field and
@@ -79,10 +91,13 @@ class CommonUtils {
         }
     }
 
-    async log_in(page, credentials) {
+    async log_in(page, credentials = null) {
         console.log("Logging in");
         await page.goto(this.realm_url + 'login/');
         assert.equal(this.realm_url + 'login/', page.url());
+        if (credentials === null) {
+            credentials = test_credentials.default_user;
+        }
         // fill login form
         const params = {
             username: credentials.username,
@@ -108,10 +123,177 @@ class CommonUtils {
         assert(page.url().includes('/login/'));
     }
 
+    async turn_off_press_enter_to_send(page) {
+        await page.$eval("#enter_sends", (el) => {
+            if (el.checked) {
+                el.click();
+            }
+        });
+    }
+
+    async wait_for_message_fully_processed(page, content) {
+        return await page.evaluate((content) => {
+            /*
+                The tricky part about making sure that
+                a message has actually been fully processed
+                is that we'll "locally echo" the message
+                first on the client.  Until the server
+                actually acks the message, the message will
+                have a temporary id and will not have all
+                the normal message controls.
+
+                For the Casper tests, we want to avoid all
+                the edge cases with locally echoed messages.
+
+                In order to make sure a message is processed,
+                we use internals to determine the following:
+                    - has message_list even been updated with
+                      the message with out content?
+                    - has the locally_echoed flag been cleared?
+
+                But for the final steps we look at the
+                actual DOM (via JQuery):
+                    - is it visible?
+                    - does it look to have been
+                      re-rendered based on server info?
+            */
+            const last_msg = current_msg_list.last();
+
+            if (last_msg.raw_content !== content) {
+                return false;
+            }
+
+            if (last_msg.locally_echoed) {
+                return false;
+            }
+
+            const row = rows.last_visible();
+
+            if (rows.id(row) !== last_msg.id) {
+                return false;
+            }
+
+            /*
+                Make sure the message is completely
+                re-rendered from its original "local echo"
+                version by looking for the star icon.  We
+                don't add the star icon until the server
+                responds.
+            */
+            return row.find('.star').length === 1;
+        }, content);
+    }
+
+    // Wait for any previous send to finish, then send a message.
+    async send_message(page, type, params) {
+        // If a message is outside the view, we will skip
+        // validation later.
+        const outside_view = params.outside_view;
+        delete params.outside_view;
+
+        await page.waitForSelector('#compose-send-button:enabled');
+        await page.waitForSelector('#compose-textarea');
+
+        if (type === "stream") {
+            await page.keyboard.press('KeyC');
+        } else if (type === "private") {
+            await page.keyboard.press("KeyX");
+            const recipients = params.recipient.split(', ')
+            for(let i=0; i < recipients.length; i+=1)
+                await this.set_pm_recipient(page, recipients[i]);
+            delete params.recipient;
+        } else {
+            assert.fail("`send_message` got invalid message type");
+        }
+
+        if (params.stream) {
+            params.stream_message_recipient_stream = params.stream;
+            delete params.stream;
+        }
+
+        if (params.subject) {
+            params.stream_message_recipient_topic = params.subject;
+            delete params.subject;
+        }
+
+        await this.fill_form(page, 'form[action^="/json/messages"]', params);
+        await this.turn_off_press_enter_to_send(page);
+        await page.waitForSelector("#compose-send-button", {visible: true})
+        await page.click('#compose-send-button');
+
+        // confirm if compose box is empty.
+        await page.waitForSelector('#compose-textarea')
+        const compose_box_element = await page.$("#compose-textarea");
+        const compose_box_content = await page.evaluate(element => element.textContent, compose_box_element);
+        assert.equal(compose_box_content, '', 'Compose box not empty after message sent');
+
+        if (!outside_view) {
+            await this.wait_for_message_fully_processed(page, params.content);
+        }
+
+        page.evaluate(() => {
+            compose_actions.cancel();
+        });
+
+        this.last_send_or_update = this.timestamp();
+    }
+
+    async send_multiple_messages(page, msgs) {
+        for (let msg_index = 0; msg_index<msgs.length; msg_index+=1){
+            let msg = msgs[msg_index]
+            await this.send_message(
+                page,
+                msg.stream !== undefined ? 'stream' : 'private',
+                msg
+            );
+        }
+    }
+
+    async get_rendered_messages(page, table) {
+
+        const messages = await page.evaluate((table) => {
+            const $ = window.$;
+            const tbl = $('#' + table);
+            return {
+                headings: $.map(tbl.find('.recipient_row .message-header-contents'), function (elem) {
+                    const $clone = $(elem).clone(true);
+                    $clone.find(".recipient_row_date").remove();
+
+                    return $clone.text().trim().replace(/\s+/g, ' ');
+                }),
+
+                bodies: $.map(tbl.find('.message_content'), function (elem) {
+                    return elem.innerHTML;
+                }),
+            };
+        }, table);
+        return messages;
+    }
+
+    // Call get_rendered_messages and then check that the last few
+    // headings and bodies match the specified arrays.
+    async expected_messages(page, table, headings, bodies) {
+        await page.waitForSelector('#' + table);
+
+        const msg = await this.get_rendered_messages(page, table);
+
+        assert.deepStrictEqual(
+            msg.headings.slice(-headings.length), headings,
+            "Didn't get expected message headings"
+        );
+
+        assert.deepStrictEqual(
+            msg.bodies.slice(-bodies.length),
+            bodies,
+            "Didn't get expected message bodies"
+        );
+    }
+
     async run_test(test_function) {
         // Pass a page instance to test so we can take
         // a screenshot of it when the test fails.
         const page = await this.get_page();
+        await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
         try {
             await test_function(page);
         } catch (e) {
@@ -126,6 +308,10 @@ class CommonUtils {
         } finally {
             this.browser.close();
         }
+    }
+
+    normalize_spaces(str) {
+        return str.replace(/\s+/g, ' ');
     }
 }
 
---
+++ frontend_tests/puppeteer_tests/02-site.js
@@ -0,0 +1,63 @@
+const assert = require("assert").strict;
+const common = require('../puppeteer_lib/common');
+
+async function site_tests(page){
+    await common.log_in(page);
+    await page.waitForSelector("#zhome", {visible: true})
+    console.log("Sanity-checking existing messages");
+    const msg = await common.get_rendered_messages(page, 'zhome');
+
+    msg.headings.forEach((heading) => {
+        assert.match(common.normalize_spaces(heading),
+                     /(^You and )|( )/,
+                     "Heading isn't well-formed");
+    });
+
+    console.log('Sending messages');
+    await common.send_multiple_messages(page, [
+        { stream: 'Verona', subject: 'frontend test',
+          content: 'test verona A' },
+
+        { stream: 'Verona', subject: 'frontend test',
+          content: 'test verona B' },
+
+        { stream: 'Verona', subject: 'other subject',
+          content: 'test verona C' },
+
+        { recipient: 'cordelia@zulip.com, hamlet@zulip.com',
+          content: 'personal A' },
+
+        { recipient: 'cordelia@zulip.com, hamlet@zulip.com',
+          content: 'personal B' },
+
+        { recipient: 'cordelia@zulip.com',
+          content: 'personal C' }
+    ]);
+
+    await common.expected_messages(page, 'zhome', [
+        'Verona > frontend test',
+        'Verona > other subject',
+        'You and Cordelia Lear, King Hamlet',
+        'You and Cordelia Lear',
+    ], [
+        '<p>test verona A</p>',
+        '<p>test verona B</p>',
+        '<p>test verona C</p>',
+        '<p>personal A</p>',
+        '<p>personal B</p>',
+        '<p>personal C</p>',
+    ]);
+
+    console.log("Sending more messages.");
+    await common.send_multiple_messages(page, [
+        { stream: 'Verona', subject: 'frontend test',
+          content: 'test verona D' },
+    
+        { recipient: 'cordelia@zulip.com, hamlet@zulip.com',
+          content: 'personal D' },
+    ]);
+
+    await common.log_out(page);
+}
+
+common.run_test(site_tests);
